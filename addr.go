e4c878f0dd76321d8330843974505cf6261a5d0c
shynome <shynome@gmail.com>
2022-10-01 16:36:05 +0800

    now keep addr empty

--- addr.go
+++ addr.go
@@ -1,23 +1,11 @@
 package wl
 
 import (
-	"fmt"
 	"net"
-
-	"github.com/pion/webrtc/v3"
-)
-
-type PairKey uint
-
-const (
-	PairLocal PairKey = iota
-	PairRemote
 )
 
 type Addr struct {
-	ICETransport *webrtc.ICETransport
-	PairKey      PairKey
-	ID           *uint16
+	Label string
 }
 
 var _ net.Addr = &Addr{}
@@ -27,19 +15,5 @@ func (a *Addr) Network() string {
 }
 
 func (a *Addr) String() string {
-	var addr string
-	if a.ICETransport != nil {
-		if pair, err := a.ICETransport.GetSelectedCandidatePair(); err == nil {
-			switch a.PairKey {
-			case PairLocal:
-				addr = pair.Local.String()
-			case PairRemote:
-				addr = pair.Remote.String()
-			}
-		}
-	}
-	if a.ID != nil {
-		addr = fmt.Sprintf("(%v) %s", *a.ID, addr)
-	}
-	return addr
+	return a.Label
 }
